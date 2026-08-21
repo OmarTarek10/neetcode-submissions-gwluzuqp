a418004fb8c1be5d905d940e1c80180aa2fd2bf3
Omar Tarek <134406845+OmarTarek10@users.noreply.github.com>
2026-08-21 12:40:11 -0400

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        string path;
+        int o=0;
+        int c = 0;
+        dfs(path, n,res,o,c);
+        return res;
+    }
+
+    void dfs(string &path, int &n, vector<string> &res, int &o, int &c){
+        if(o == n && c == n){
+            res.push_back(path);
+            return;
+        }
+        else if(c > o){
+            return;
+        }
+
+        if(o < n){
+            path += '(';
+            o++;
+            dfs(path, n, res,o,c);
+            path.pop_back();
+            o--;
+        }
+        if(c < n){
+            path += ')';
+            c++;
+            dfs(path, n, res,o,c);
+            c--;
+            path.pop_back();
+        }
+
+    }
+};
